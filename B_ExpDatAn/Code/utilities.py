c7efdf0ce4d2662c4ce706fe29322a264a72f2c5
Iris Hinrichs <iris.hinrichs@iu-study.org>
2024-07-19 16:10:09 +0200

    Added function 'get_filestring'

--- B_ExpDatAn/Code/utilities.py
+++ B_ExpDatAn/Code/utilities.py
@@ -5,6 +5,33 @@ Created on Thu Jul 18 14:57:32 2024
 @author: bd2107
 """
 import pandas as pd
+import datetime as dt
+
+
+def get_filestring(s='Nordsee II',p='WT',start=dt.datetime(2020,1,1), end=dt.datetime(2024,6,30)):
+    '''
+    Make string of file name based on stationname, parameter name, start date
+    and end date of data
+
+    Parameters
+    ----------
+    s : str
+        station name. Default is 'Nordsee II'
+    p : str
+        parameter name. Default is 'WT'
+    start : datetime.datetime
+        start of time series, default is dt.datetime(2020,1,1).
+    end : datetime.datetime
+        end of time series, default is end=dt.datetime(2024,6,30).
+
+    Returns
+    -------
+    filestring : str
+        file name .
+
+    '''
+    filestring = '_'.join([s.replace(' ','_'),start.strftime('%Y%m%d'), end.strftime('%Y%m%d'), p,'all_.csv'])
+    return filestring
 
 def read_station_data(filestr):
     usecols=['TIME_VALUE','Z_LOCATION','DATA_VALUE', 'QF1', 'QF3']
