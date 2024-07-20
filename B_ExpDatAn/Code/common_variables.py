0b9682baeb720659ba4c460aa0200f20a6a6fbe9
Iris Hinrichs <iris.hinrichs@iu-study.org>
2024-07-20 13:32:56 +0200

    Changed figure size variables

--- B_ExpDatAn/Code/common_variables.py
+++ B_ExpDatAn/Code/common_variables.py
@@ -8,6 +8,8 @@ Created on Fri Jul 19 16:11:41 2024
 Variables common for several scripts
 '''
 import datetime as dt
+from matplotlib import pyplot as plt
+
 
 # Path to original data
 datapath = '../../A_Data/'
@@ -25,15 +27,18 @@ params = ['WT', 'SZ']
 paramdict = {'WT': 'Wassertemperatur [° C]', 'SZ':'Salzgehalt []'}
 
 # temporal limits
-tlims = [dt(2020,1,1,0,0),dt(2024,6,3,23,59)]
+tlims = [dt.datetime(2020,1,1,0,0),dt.datetime(2024,6,30,23,59)]
 
 
 # variables for customizing figures 
 cm = 1/2.54  # conversion factor centimeters=>inches
-dpi=300
-layout = 'constrained'
 
-#fig = plt.figure()
+# set figwidth and dpi for all figures
+plt.rcParams["figure.figsize"][0] = 16.5*cm # figure width
+plt.rcParams["figure.dpi"] = 300
+layout = 'constrained'
+bbox_inches = 'tight'
+
 # fontdict for text in figure
 fs = 10
 fontdict = {'family': ['sans-serif'],
