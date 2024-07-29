f7772022c78e816a4a5c4853dcfae5c4d55c1e63
bm4181 <iris.hinrichs@bsh.de>
2024-07-29 15:36:31 +0200

    Moved function 'statistics' to 'explore_anomalies.py'

--- B_ExpDatAn/Code/time_series_statistics.py
+++ B_ExpDatAn/Code/time_series_statistics.py
@@ -18,53 +18,6 @@ from common_variables import datapath, layout, cm, stations, stationsdict,\
 import os
     
 
-def statistics(stname='North Sea Buoy II', paracode='WT', dlevels='all', 
-            start=tlims[0], end=tlims[1]):
-    '''
-    Gain statistics of parameter time series and print them.
-
-    Parameters
-    ----------
-    stname : str, optional
-        Name of the station. 
-        The default is 'North Sea Buoy II'.
-    paracode : str, optional
-        Code of observed parameter. The default is 'WT'.
-    dlevels : str of list, optional
-        string or tuple of depth levels. The default is 'all'.
-    start : datetime.datetime, optional
-        Start of time series. The default is dt.datetime(2020,1,1).
-    end : datetime.datetime, optional
-        End of time series. The default is dt.datetime(2024,4,30).
-
-    Returns
-    -------
-    None.
-
-    '''
-    
-    # number of possible quality flags
-    n_qfs = 4
-     # %% define path and filename
-    strings = get_filestring(stname)
-    file = datapath+strings
-    df = read_station_data(file)
-    
-   
-    n_dp=len(df)
-    print(stname+', '+paracode+'\n'+
-          '----------------------------------\n'+
-         'Anzahl Datenpunkte ('+start.strftime('%Y%m%d')+'-'+end.strftime('%Y%m%d')+'): '+str(n_dp)+'\n')
-    for flag in range(0,n_qfs+1):
-        absval=df[df.QF1==flag].QF1.count()
-        frac = absval/n_dp
-        print('Validation level 1, quality flag '+str(flag)+': '+str(absval)+' ({:.4f}'.format(frac)+')')
-        absval=df[df.QF3==flag].QF3.count()
-        frac = absval/n_dp
-        print('Validation level 3, quality flag '+str(flag)+': '+str(absval)+' ({:.4f}'.format(frac)+')')
-    print('----------------------------------\n')
-    return
-
 def plot_coverage():
     # variables related to figure
     plt.rcParams['figure.figsize'][1]=10*cm
