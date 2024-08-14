31a6f8af34bece2d05fca76a45f6da5a4e3b2bd7
Iris Hinrichs <iris.hinrichs@iu-study.org>
2024-08-14 21:06:39 +0200

    first attempt to visualize bathymetry and stations in figure

--- Misc/map_stations.py
+++ Misc/map_stations.py
@@ -12,6 +12,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+import netCDF4
+
 # from common_variables import  layout, cm, stationsdict,\
      # fs, fontdict, bbox_inches
      
@@ -21,21 +23,33 @@ import numpy as np
 def map_stations(lat= [54.5981666667,54.4995, 54.9983333333, 54.6827833333],\
                  lon=[11.1491666667,10.2736666667, 6.3515, 6.75445]):
     
-    # define everything that has to do with the resulting figure
-    # plt.rcParams['figure.figsize'][1]=10*cm
-    # fig = plt.figure(layout=layout)
-    # savefigpath = '../Figures/temporal_coverage.png'
-    dlat = 1
-    dlon = 1
+     # define everything that has to do with the resulting figure
+     # plt.rcParams['figure.figsize'][1]=10*cm
+     # fig = plt.figure(layout=layout)
+     # savefigpath = '../Figures/temporal_coverage.png'
+     dlat = 1
+     dlon = 1
 
-    # load GEBCO
+     # load GEBCO
+     gebcofile='Misc\GEBCO_14_Aug_2024_1671b01b7c83\gebco_2024_n67.0_s50.0_w10.0_e30.0.nc'
+     rg = netCDF4.Dataset(gebcofile, 'r',format="NETCDF4")
+     elev = rg['elevation'][:]
+     glat =rg['lat'][:]
+     glon = rg['lon'][:]
+     rg.close()
 
-    # Add station positions
-    plt.plot(lon, lat, '.')
+     # create landmask
+     landmask = (elev>=0)
+     
+     # plot elevation
+     plt.imshow(np.flipud(elev), interpolation=None, extent=[min(glon), max(glon), min(glat), max(glat)]) 
 
+     # plot landmask
+     plt.imshow(np.flipud(landmask), cmap='gist_gray', alpha=0.2, extent=[min(glon), max(glon), min(glat), max(glat)])
+
+     # Add station positions
+     plt.plot(lon, lat, '.')
+     plt.show()
     
-    
-    plt.show()
-    
-    return 
+     return 
 map_stations()
