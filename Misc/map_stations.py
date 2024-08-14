9bcfe843dfdd4b418dcc8ea6d953389167d34f9d
iris.hinrichs <iris.hinrichs@bsh.de>
2024-08-14 15:57:03 +0200

    Swittch to GEBCO bathymetry

--- Misc/map_stations.py
+++ Misc/map_stations.py
@@ -6,15 +6,16 @@ Created on Mon Aug 12 15:01:42 2024
 """
 
 
-#import geopandas as gpd
-#from shapely.geometry import LineString, Point
-import cartopy.crs as ccrs
-#import pandas as pd
+
+
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+
 # from common_variables import  layout, cm, stationsdict,\
      # fs, fontdict, bbox_inches
      
-#https://scitools.org.uk/cartopy/docs/latest/gallery/lines_and_polygons/ocean_bathymetry.html
+
 
 
 def map_stations(lat= [54.5981666667,54.4995, 54.9983333333, 54.6827833333],\
@@ -26,26 +27,15 @@ def map_stations(lat= [54.5981666667,54.4995, 54.9983333333, 54.6827833333],\
     # savefigpath = '../Figures/temporal_coverage.png'
     dlat = 1
     dlon = 1
-    
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.stock_img()
-   
 
-    # # turn positions into shapely.LineString object
-    # if len(lat) >1 and len(lon)>1:
-    #     shobj = LineString(list(zip(lon, lat)))
-    # else:
-    #     shobj = Point((lon[0],lat[0]))
+    # load GEBCO
 
-    # plot map with regions and position values
-    # ax = world.plot(
-        # color=[0.8,0.8,0.8])
+    # Add station positions
     plt.plot(lon, lat, '.')
-    plt.xlim([min(lon)-dlon, max(lon)+dlon])
-    plt.ylim([min(lat)-dlat, max(lat)+dlat])
-    plt.grid()
+
+    
+    
     plt.show()
     
     return 
-
 map_stations()
