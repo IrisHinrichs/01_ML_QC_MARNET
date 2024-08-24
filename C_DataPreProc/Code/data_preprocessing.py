c5e8a370dbe0e86c9803a0309919077d824ed397
Iris Hinrichs <iris.hinrichs@iu-study.org>
2024-08-24 13:22:38 +0200

    Addes documentation to function 'piecewise_interpolation'

--- C_DataPreProc/Code/data_preprocessing.py
+++ C_DataPreProc/Code/data_preprocessing.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import datetime as dt
+#import datetime as dt
 import pandas as pd
 import numpy as np
 import sys
@@ -8,20 +8,22 @@ import os
 
 abspath = os.path.abspath("B_ExpDatAn/Code")
 sys.path.insert(0, abspath)
-from time_series_statistics import find_all_time_spans
+from time_series_statistics import find_all_time_spans  # noqa: E402
 
 
 def piecewise_interpolation(ts, gap=10):
     '''
     Piecewise linear interpolation of MARNET time series data.
-    Start and end point of time series' pieces are defined by 
-    parameter 'gap': temporal gaps less or equal 'gap' are ignored
+    Start and end point of parts of the time series are defined by 
+    parameter 'gap': temporal gaps less or equal 'gap' are ignored, 
+    only those greater than 'gap' mark the end and the beginning of
+    a piece of the time series. 
 
     Parameters
     ----------
     ts : pandas.core.DataFrame
-        Time series data consisting of the datestamps as index and 
-        the actual data values plus quality flags. 
+        Time series data consisting of the datestamps as index, 
+        the depth levels and the  actual data values plus quality flags . 
 
     gap : int
         Integer defining the maximum length  of temporal gaps in hours
@@ -31,7 +33,9 @@ def piecewise_interpolation(ts, gap=10):
     -------
     ts_interp : pandas.core.DataFrame
         Interpolated time series. Data gaps less or equal 'gap' are
-        interpolated, others are void.
+        interpolated, others are void. All columns of ts are in ts_interp
+        plus an additional column, INTERP, showing if corresponding values
+        are interpolated (INTERP=True) or the original ones (INTERP=False)
 
     '''
     # find all time spans that only have temporal gaps 
@@ -45,34 +49,42 @@ def piecewise_interpolation(ts, gap=10):
     for c in ts.columns:
         exec(c+"=[]")
     INTERP = []
-    for p in time_spans.index[0:-2]: # iterate over all parts of the time series
+
+    # iterate over all parts of the time series
+    for p in time_spans.index:
+        # define time stamps for current part of
+        # time series
         end = p+time_spans[p]
         start=pd.Timestamp(p.year, p.month, p.day, p.hour, 0)
         new_p_index =  pd.date_range(start, end, freq='h')
-        old_inds = np.where(((ts.index >= start)&(ts.index<=end))==True)
+
+        # get existing time stamps for part of time series
+        old_inds = np.where(((ts.index >= start)&(ts.index<=end)))
         old_p_index=ts.index[old_inds[0]]
-        if 'index_col' in locals():
-            index_col=index_col.append(new_p_index)
-        else:
+
+        # concatenate all new time stamps to a datetimeindex
+        if 'index_col' in locals(): # variable for index exists already
+            index_col=index_col.append(new_p_index)  # noqa: F821
+        else: # variable for index is created
             index_col= new_p_index
+
+        # Interpolate values for all other columns of data frame
         for c in list(ts.columns):
             exec(c+"+=list(np.interp(new_p_index, ts.index, ts."+c+"))")
+        
+        # Create information about interpolated values of
+        # current piece of time series and concat it to single list
         interp_bool = [True]*len(new_p_index)
         int_inds = [list(new_p_index).index(oi) for oi in list(old_p_index) if oi in list(new_p_index)]
         if len(int_inds)!=0:
             for i in int_inds:
                 interp_bool[i]=False 
         INTERP+=interp_bool
+
     # fill data frame
     ts_interp['TIME_VALUE']=index_col
-    ts_interp=ts_interp.set_index(keys='TIME_VALUE', drop=True)
+    ts_interp=ts_interp.set_index(keys='TIME_VALUE', drop=True) # set DatetimeIndex as index
     
     for c in ts_interp.columns:
         exec("ts_interp."+c+"="+c)
     return ts_interp
-from utilities import get_filestring, read_station_data
-from common_variables import datapath
-filestr = get_filestring()
-data = read_station_data(datapath+filestr)
-ts = data[data.Z_LOCATION==-3]
-ts_interp = piecewise_interpolation(ts)
