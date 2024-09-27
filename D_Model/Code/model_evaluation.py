07553dc7aba8f756c0d16b13f761ec1308627e72
Iris Hinrichs <iris.hinrichs@iu-study.org>
2024-09-27 18:12:16 +0200

    Created ROC metrics

--- D_Model/Code/model_evaluation.py
+++ D_Model/Code/model_evaluation.py
@@ -5,6 +5,8 @@ import pandas as pd
 import sys
 import datetime as dt
 import  numpy as np
+from sklearn.metrics import roc_curve, roc_auc_score
+from matplotlib import pyplot as plt
 
 # Add absolute path of directory 
 # 01_ML_QC_MARNET to sys.path
@@ -17,13 +19,30 @@ sys.path.insert(0,currentdir)
 
 
 from B_ExpDatAn.Code.utilities import get_filestring, read_station_data  # noqa: E402
-from B_ExpDatAn.Code.common_variables import stations, params, tlims, stationsdict  # noqa: E402
+from B_ExpDatAn.Code.common_variables import (  # noqa: E402
+    stations,
+    params,
+    tlims,
+    stationsdict,
+    paramdict,
+    layout,
+    cm,
+    fs,
+    fontdict,
+    bbox_inches,
+)  
+from D_Model.Code.detect_anomalies import methods  # noqa: E402
 
 # from where to load results
 log_file = 'log_training.txt'
 
-# where to save dataframe
+# where to save dataframe of training results
 savepath = os.path.join(currentdir,'D_Model', 'Trained_Models', 'Ocean_WNN')
+
+# where to save results from evaluation
+evalpath = os.path.join(currentdir, 'D_Model','Evaluation', 'Figures')
+
+mthds = {'Median-Methode': 'ad_mm', 'Ocean_WNN': 'ad_ownn'}
 
 def get_numerics(s)  -> list:
     replace = [
@@ -71,7 +90,8 @@ def get_fit_res(lgfile) -> dict:
     model_dict={'n_epochs':n_epochs, 't_loss':tloss, 'v_loss':vloss, 'thresh':thresh}
     return model_dict
 
-def main():
+def summarize_model_fitting():
+    '''Summarizes the results of oceanwnn model fitting'''
     model_data = []
     
     for st in stations:
@@ -120,6 +140,60 @@ def main():
         ],
     )
    # save results of model fitting
-    model_fit.to_csv(os.path.join(savepath, 'results_model_fitting.csv'))    
-if __name__=='main':   
+    model_fit.to_csv(os.path.join(savepath, 'results_model_fitting.csv')) 
+
+def calc_roc_metrics(ts, method):
+    y_score = ts[method].values
+    y_true = ts.QF3.values
+    y_true[y_true==2]=0
+    y_true[y_true>2]=1
+    y_true_s = y_true[np.where(np.isnan(y_score)==False)]
+    if all(np.iszero(y_true_s)):
+        fpr=tpr=threshs=auc=np.nan
+        return fpr,tpr,threshs,auc
+    y_score_s = y_score[np.where(np.isnan(y_score)==False)]
+    auc = roc_auc_score(y_true_s, y_score_s)
+    fpr,tpr,threshs = roc_curve(y_true_s, y_score_s)
+    return fpr,tpr,threshs, auc
+def main():
+    '''Plot ROC metrics'''
+    
+    for st in stations:
+        for p in params:
+            filestr = get_filestring(st, p, tlims[0], tlims[1])
+            filestr = filestr.replace('.csv','_'+ methods+'.csv')
+            filestr = filestr.replace('A_Data', os.path.join('D_Model', 'Results'))
+            data=pd.read_csv(filestr)
+            unique_d=list(set(data.Z_LOCATION))
+            unique_d.sort(reverse=True)
+            legendstrings = []
+            savefigpath = os.path.join(evalpath, stationsdict[st].replace(" ", "_"), p)
+            if not os.path.isdir(savefigpath):
+                    os.makedirs(savefigpath) 
+            for d in unique_d:
+                depth = abs(d)
+                figname = str(depth)+'m_ROCmetrics.png'
+                fig = plt.figure(layout=layout)
+                ts = data[data["Z_LOCATION"]==d] # entries corresponding to depth level d
+                for m in mthds:
+                    fpr, tpr, threshs,auc = calc_roc_metrics(ts, mthds[m])
+                    if all(np.isnan([fpr, tpr, threshs,auc])):
+                        
+                        continue
+                    plt.plot(fpr, tpr, '.')
+                    legendstrings.append(m+', AUC='+str(round(auc,2)))
+                plt.xlabel('FPR')
+                plt.ylabel('TPR')
+                plt.legend(legendstrings)
+                titlestring = stationsdict[st]+', '+\
+                    str(depth)+' m,'+\
+                        paramdict[p].replace('[°C]', '')
+                                    
+                plt.title(titlestring, fontsize=fs)
+                plt.legend(legendstrings)
+                #plt.show()
+                fig.savefig(os.path.join(savefigpath,figname), bbox_inches=bbox_inches)
+                plt.close(fig) 
+
+if __name__=='__main__':   
     main()
