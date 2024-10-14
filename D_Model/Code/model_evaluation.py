6a6da19fa11f44daf1d8bf03d192526897bbc186
Iris Hinrichs <iris.hinrichs@iu-study.org>
2024-10-14 20:48:57 +0200

    Minor changes

--- D_Model/Code/model_evaluation.py
+++ D_Model/Code/model_evaluation.py
@@ -46,7 +46,7 @@ from D_Model.Code.ocean_wnn.algorithm_iris import CustomParameters as ownn_custP
 log_file = 'log_file.txt'
 
 # differencing parameter
-ddiff = 2
+ddiff = 0
 
 # where to save dataframe of training results
 savepath = os.path.join(
@@ -818,8 +818,8 @@ def choose_best_model():
 
 if __name__=='__main__':   
     # summarize_model_fitting()
-    predictions_observations()
-    # plot_roc_metrics()
+    # predictions_observations()
+    plot_roc_metrics()
     #plot_auc_roc_summary()
     #plot_mase_summary()
     # model_cross_validation()
