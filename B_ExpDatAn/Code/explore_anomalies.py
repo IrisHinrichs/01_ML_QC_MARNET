52c88f3b5417e62e734bc24aa9974e1701cadcdb
Iris Hinrichs <iris.hinrichs@iu-study.org>
2024-10-26 16:43:23 +0200

    Minor changes

--- B_ExpDatAn/Code/explore_anomalies.py
+++ B_ExpDatAn/Code/explore_anomalies.py
@@ -6,16 +6,42 @@ Created on Mon Jul 29 15:32:37 2024
 """
 import math
 import datetime as dt
-import pandas as pd # version 2.1.4 auf dem Laptop zu Hause, 1.4.4 bei der Arbeit
-from B_ExpDatAn.Code.utilities import datapath, read_station_data, get_filestring, diff_time_vec, convert_duration_string
-from matplotlib import pyplot as plt 
+import pandas as pd 
+from matplotlib import pyplot as plt
+from matplotlib.legend_handler import HandlerTuple 
 import matplotlib.dates as mdates
-
-
-from B_ExpDatAn.Code.common_variables import  layout, cm, stations, stationsdict,\
-    params, paramdict, tlims, fs, fontdict, bbox_inches
-    
 import os
+import sys
+
+# Add absolute path of directory 
+# 01_ML_QC_MARNET to sys.path
+currentdir=os.path.dirname(__file__)
+pathpieces = os.path.split(currentdir)
+while pathpieces[-1]!='01_ML_QC_MARNET':
+    currentdir= os.path.dirname(currentdir)
+    pathpieces = os.path.split(currentdir)
+sys.path.insert(0,currentdir)
+
+
+from B_ExpDatAn.Code.common_variables import (  # noqa: E402
+    layout,
+    cm,
+    stations,
+    stationsdict,
+    params,
+    paramdict,
+    tlims,
+    fs,
+    fontdict,
+    bbox_inches,
+)  # noqa: E402
+from B_ExpDatAn.Code.utilities import (  # noqa: E402
+    datapath,
+    read_station_data,
+    get_filestring,
+    diff_time_vec,
+    convert_duration_string,
+)
 
 
 def statistics(stname='North Sea Buoy II', paracode='WT', dlevels='all', 
